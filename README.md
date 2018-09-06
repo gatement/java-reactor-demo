304fd459bac8dfa1bc4d3498f59c45a135cea84a
JohnsonLau <gatement@gmail.com>
2018-09-07 01:02:47 +0800

    go

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+## reference
+http://blog.51cto.com/liukang/2090191
+
 ## run
 mvn compile exec:java -Dexec.mainClass=net.johnsonlau.reactor.demo.App
--- src/main/java/net/johnsonlau/reactor/demo/App.java
+++ src/main/java/net/johnsonlau/reactor/demo/App.java
@@ -3,10 +3,29 @@ package net.johnsonlau.reactor.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public class App {
 	private static final Logger logger = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) {
-		logger.info("=============go: {}", "johnson");
+		logger.info("=============");
+
+		Flux.just(1, 2, 3, 4, 5, 6).subscribe(System.out::print);
+		System.out.println();
+
+		Mono.just(1).subscribe(System.out::println);
+
+		Flux.just(1, 2, 3, 4, 5, 6).subscribe(
+			    System.out::println,
+			    System.err::println,
+			    () -> System.out.println("Completed!"));
+		
+		Mono.error(new Exception("some error")).subscribe(
+		        System.out::println,
+		        System.err::println,
+		        () -> System.out.println("Completed!")
+		);
 	}
 }
