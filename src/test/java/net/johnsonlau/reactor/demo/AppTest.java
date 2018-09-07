2d22dbe386228fb01dd270165681f3ea39193e64
JohnsonLau <gatement@gmail.com>
2018-09-08 01:18:16 +0800

    go

--- src/test/java/net/johnsonlau/reactor/demo/AppTest.java
+++ src/test/java/net/johnsonlau/reactor/demo/AppTest.java
@@ -1,5 +1,26 @@
 package net.johnsonlau.reactor.demo;
 
-public class AppTest {
+import org.junit.Test;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class AppTest {
+	private Flux<Integer> generateFluxFrom1To6() {
+	    return Flux.just(1, 2, 3, 4, 5, 6);
+	}
+	private Mono<Integer> generateMonoWithError() {
+	    return Mono.error(new Exception("some error"));
+	}
+	@Test
+	public void testViaStepVerifier() {
+	    StepVerifier.create(generateFluxFrom1To6())
+	            .expectNext(1, 2, 3, 4, 5, 6)
+	            .expectComplete()
+	            .verify();
+	    StepVerifier.create(generateMonoWithError())
+	            .expectErrorMessage("some error")
+	            .verify();
+	}
 }
